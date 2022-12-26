64f8dc635c8bdaa47fb626ac180203a34bbf7566
Emanuele Roppo <ema.roppo@gmail.com>
2022-12-26 18:55:14 +0100

    Added restock and fulfill order method, rationalized connections to database (cursor and connection are now an attribute of the inventory object, rather than a new connection being created  with each method), added  __del__ method to close connection

--- db_manager.py
+++ db_manager.py
@@ -45,68 +45,80 @@ def setup_db(db_path="inventory.db"):
 class Inventory:
     def __init__(self, db_path="inventory.db"):
         self.db_path = db_path
+        self.conn = sqlite3.connect(self.db_path)
+        self.c = self.conn.cursor()
 
     def add_category(self, category_name):
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        c.execute(
+        self.c.execute(
             """INSERT INTO categories(category_name)
             VALUES (?)""",
             (category_name,),
         )
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
     def add_product(self, product_name, category_name):
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        c.execute(
+
+        self.c.execute(
             """INSERT INTO products(product_name, category_id)
             VALUES (?, (SELECT category_id FROM categories WHERE category_name = ?))""",
             (product_name, category_name),
         )
-        conn.commit()
-        conn.close()
+        self.conn.commit()
+        self.conn.close()
 
     def add_store(self, street_n, street_name, city, ZIP):
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
         try:
-            c.execute(
+            self.c.execute(
                 """INSERT INTO stores(street_n, street_name, city, ZIP)
                 VALUES (?, ?, ?, ?)""",
                 (street_n, street_name, city, ZIP),
             )
         except sqlite3.IntegrityError:
             print("Store already exists")
-        conn.commit()
-        conn.close()
+        self.conn.commit()
 
-    def add_stock(self, product_name, store_id, qty):
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        c.execute(
-            """INSERT INTO stock(product_id, store_id, qty)
-            VALUES ((SELECT product_id FROM products WHERE product_name = ?), ?, ?)""",
-            (product_name, store_id, qty),
-        )
-        conn.commit()
-        conn.close()
+    def restock(self, restock_file):
+        # read restock_file
+        with open(restock_file, "r") as f:
+            restock_lines = f.read().splitlines()
 
-    def update_stock(self, product_name, store_id, qty):
-        conn = sqlite3.connect(self.db_path)
-        c = conn.cursor()
-        try:
-            c.execute(
-                """UPDATE stock
-                SET qty = ?
-                WHERE product_id = (SELECT product_id FROM products WHERE product_name = ?)
-                AND store_id = ?""",
-                (qty, product_name, store_id),
+        # for each line in restock_file, if the store_id and product_id are not in stock, add them, otherwise update the qty by adding the new qty to the old qty
+
+        for line in restock_lines:
+            store_id, product_name, qty = line.split(",")
+            self.c.execute(
+                """INSERT INTO stock(product_id, store_id, qty)
+                VALUES ((SELECT product_id FROM products WHERE product_name = ?), ?, ?)
+                ON CONFLICT (product_id, store_id) DO UPDATE SET qty = stock.qty + ?""",
+                (product_name, store_id, qty, qty),
             )
-        except sqlite3.OperationalError:
-            print("Product not in store")
+        self.conn.commit()
+        self.conn.close()
+
+    def fulfill_order(self, order_file="restock.csv"):
+        # read order_file
+        with open(order_file, "r") as f:
+            order_lines = f.read().splitlines()
+
+        # for each line in order_file, if the store_id and product_id are in stock, update the qty by subtracting the new qty from the old qty
+        try:
+            for line in order_lines:
+                store_id, product_name, qty = line.split(",")
+                self.c.execute(
+                    """UPDATE stock SET qty = stock.qty - ?
+                    WHERE product_id = (SELECT product_id FROM products WHERE product_name = ?)
+                    AND store_id = ?""",
+                    (qty, product_name, store_id),
+                )
+            self.conn.commit()
         except sqlite3.IntegrityError:
-            print("Product out of stock")
-        conn.commit()
-        conn.close()
+            print(f"{product_name}: Not enough stock")
+
+        except sqlite3.OperationalError:
+            print(f"{product_name}: Product not in stock")
+
+    def __del__(self):
+        self.conn.close()
+
+
+# TO DO: impelement check for qty in stock for all items in the order before fulfilling order
