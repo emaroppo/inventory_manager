bd6285afd0abb82cae0747cbffca2a7cd4620c7f
Emanuele Roppo <ema.roppo@gmail.com>
2023-02-02 16:28:25 +0100

    initial refactor

--- db_manager.py
+++ db_manager.py
@@ -47,7 +47,7 @@ class DBBuilder:
             """CREATE TABLE IF NOT EXISTS orders(
                 order_id INTEGER PRIMARY KEY,
                 store_id INTEGER NOT NULL,
-                status_id INTEGER NOT NULL,
+                status_id INTEGER NOT NULL DEFAULT 1,
                 FOREIGN KEY (store_id) REFERENCES stores(store_id)
                 ON UPDATE CASCADE,
                 FOREIGN KEY (status_id) REFERENCES status(status_id) ON UPDATE CASCADE 
@@ -59,7 +59,8 @@ class DBBuilder:
             order_id INTEGER NOT NULL,
             product_id INTEGER NOT NULL,
             qty INTEGER NOT NULL,
-            in_stock INTEGER BOOLEAN NOT NULL,
+            in_stock BOOLEAN NOT NULL,
+            shipped BOOLEAN NOT NULL DEFAULT 0,
             FOREIGN KEY (order_id) REFERENCE store(store_id)
             ON UPDATE CASCADE,
             FOREIGN KEY (product_id) REFERENCE products(product_id) ON UPDATE CASCADE,
@@ -161,15 +162,22 @@ class Inventory:
         self.conn.commit()
         self.c.execute(
             """SELECT order_id FROM orders
-            WHERE store_id = ? AND fulfilled = 0""",
+            WHERE store_id = ? AND status_id = 1""",
             (store_id,),
         )
         order_id = self.c.fetchone()
 
         for item in items:
+            if item[2] > self.check_instore_product_availability(
+                product_id=item[1], store_id=store_id
+            ):
+                in_stock = False
+            else:
+                in_stock = True
+
             self.c.execute(
-                """INSERT INTO order_details(order_id, product_id, qty) VALUES (?, ?, ?)""",
-                (order_id, item[1], item[2]),
+                """INSERT INTO order_details(order_id, product_id, qty, in_stock) VALUES (?, ?, ?)""",
+                (order_id[0], item[1], item[2], in_stock),
             )
 
         return order_id[0]
@@ -183,7 +191,7 @@ class Inventory:
         self.conn.commit()
         self.c.execute(
             """SELECT restock_order_id FROM restock_orders
-            WHERE store_id = ? AND fulfilled = 0""",
+            WHERE store_id = ? AND status_id = 1""",
             (store_id,),
         )
         restock_order_id = self.c.fetchone()
@@ -191,20 +199,17 @@ class Inventory:
         for item in items:
             self.c.execute(
                 """INSERT INTO restock_order_details(restock_order_id, product_id, qty) VALUES (?, ?, ?)""",
-                (restock_order_id, item[1], item[2]),
+                (restock_order_id[0], item[1], item[2]),
             )
 
         return restock_order_id[0]
 
-    def restock(self, restock_file):
-        # read restock_file
-        with open(restock_file, "r") as f:
-            restock_lines = f.read().splitlines()
+    def restock(self, order):
 
         # for each line in restock_file, if the store_id and product_id are not in stock, add them, otherwise update the qty by adding the new qty to the old qty
 
-        for line in restock_lines:
-            store_id, product_name, qty = line.split(",")
+        for item in order.items:
+            store_id, product_name, qty = item
             self.c.execute(
                 """INSERT INTO stock(product_id, store_id, qty)
                 VALUES ((SELECT product_id FROM products WHERE product_name = ?), ?, ?)
@@ -213,14 +218,31 @@ class Inventory:
             )
         self.conn.commit()
 
-    def check_instore_product_availability(self, product_name, store_id):
-        self.c.execute(
-            """SELECT qty FROM stock
-            WHERE product_id = (SELECT product_id FROM products WHERE product_name = ?)
-            AND store_id = ?""",
-            (product_name, store_id),
-        )
-        qty = self.c.fetchone()
+    def check_instore_product_availability(
+        self, store_id, product_name=None, product_id=None
+    ):
+
+        if product_id and product_name:
+            raise TypeError("Specify only 1 of product_id and product_name")
+
+        if product_name:
+            self.c.execute(
+                """SELECT qty FROM stock
+                WHERE product_id = (SELECT product_id FROM products WHERE product_name = ?)
+                AND store_id = ?""",
+                (product_name, store_id),
+            )
+            qty = self.c.fetchone()
+
+        elif product_id:
+            self.c.execute(
+                """SELECT qty FROM stock WHERE product_id = ? AND store_id= ?""",
+                (product_id, store_id),
+            )
+
+        else:
+            raise TypeError("Specify at least 1 of product_id and product_name")
+
         if qty is None:
             return 0
         else:
--- main.py
+++ main.py
@@ -1,132 +1,28 @@
-import shutil
-import pandas as pd
-from datetime import datetime
-import csv_generator as gen
-import os
+import reader
+from db_manager import Inventory, setup_db
+import sqlite3
+import random
 
-def loaded(file, dest):
-    dest_folder='./Done/{}/{}-{}'.format(dest,datetime.now().strftime("%d-%m-%Y_%H%M"),file)
-    shutil.move(file,dest_folder)
+def example_restock(items_number=10):
+    # retrieve list of store ids fro db
+    conn = sqlite3.connect("inventory.db")
+    c = conn.cursor()
+    c.execute("SELECT store_id FROM stores")
+    store_ids = c.fetchall()
 
-def add_item(item, category, qty=0):
-    new_item={}
-    new_item['Item']=item
-    new_item['Category']=category
-    new_item['Qty']=qty
-    return new_item
+    # retrieve list of product names from db
+    c.execute("SELECT product_name FROM products")
+    product_names = c.fetchall()
 
-
-def read_order(path='order.csv'):
-    inventory_df = pd.read_csv('inventory.csv')
-    unfulfilled_orders=pd.read_csv('unfulfilled.csv')
-
-    try:
-        with open(path) as file:
-            order_items=file.readlines()
-
-    except FileNotFoundError:
-        print('Order File Not Found. Please Enter File Path.')
-        path=input('File Path:')
-        with open(path) as file:
-            order_items=file.readlines()
-
-    for item in order_items:
-        item = item.split(',')
+    # write csv file with number of rows = items_numbers lines where each row is a store_id, product_, and a random qty (between 0 and 100)
+    with open("examples/restock.csv", "w") as f:
+        for i in range(items_number):
+            store_id = random.choice(store_ids)[0]
+            product_name = random.choice(product_names)[0]
+            qty = random.randint(0, 100)
+            f.write(f"{store_id},{product_name},{qty}" + "\n")
 
 
-        if item[0] not in list(inventory_df['Item']):
-            print('Item "{}" not previously in inventory. Please Specify Category.'.format(item[0]))
-            category=input('Enter Category: ')
-            inventory_df=inventory_df.append(add_item(item=item[0],category=category),ignore_index=True)
-
-        if int(inventory_df.loc[inventory_df['Item']==item[0],'Qty']) >= int(item[1]):
-            inventory_df.loc[inventory_df['Item']==item[0],'Qty']-=int(item[1])
-        else:
-            order_qty=int(item[1])-int(inventory_df.loc[inventory_df['Item']==item[0],'Qty'])
-            inventory_df.loc[inventory_df['Item']==item[0],'Qty']=0
-
-            if item[0] not in list(unfulfilled_orders['Item']):
-                unfulfilled_orders=unfulfilled_orders.append(add_item(item=item[0],category=inventory_df.loc[inventory_df['Item']==item[0],'Category'].item()), ignore_index=True)
-
-            unfulfilled_orders.loc[unfulfilled_orders['Item'] == item[0], 'Qty'] += order_qty
-
-    loaded(path,'Orders')
-    inventory_df.to_csv('inventory.csv', index=False)
-    unfulfilled_orders.to_csv('unfulfilled.csv', index=False)
-    return inventory_df
-
-
-def read_restock(path='restock.csv'):
-    inventory_df = pd.read_csv('inventory.csv')
-    unfulfilled_orders=pd.read_csv('unfulfilled.csv')
-
-    try:
-        with open(path) as file:
-            restock_items = file.readlines()
-
-    except FileNotFoundError:
-        print('Restock File Not Found. Please Enter File Path.')
-        path = input('File Path:')
-        with open(path) as file:
-            restock_items = file.readlines()
-
-    for item in restock_items:
-
-        item = item.split(',')
-
-        if item[0] not in list(unfulfilled_orders['Item']) or int(unfulfilled_orders.loc[unfulfilled_orders['Item']==item[0],'Qty']==0):
-            inventory_df.loc[inventory_df['Item'] == item[0], 'Qty']+=int(item[1])
-
-        elif int(item[1])<=int(unfulfilled_orders.loc[unfulfilled_orders['Item']==item[0],'Qty']):
-            unfulfilled_orders.loc[unfulfilled_orders['Item'] == item[0], 'Qty']-=int(item[1])
-
-        else:
-            restock_qty=int(item[1])-int(unfulfilled_orders.loc[unfulfilled_orders['Item'] == item[0], 'Qty'])
-            unfulfilled_orders.loc[unfulfilled_orders['Item'] == item[0], 'Qty']=0
-            inventory_df.loc[inventory_df['Item'] == item[0], 'Qty'] += restock_qty
-
-
-
-        if item[0] not in list(inventory_df['Item']):
-            print('Item "{}" not previously in inventory. Please Specify Category.'.format(item[0]))
-            category=input('Enter Category: ')
-            inventory_df=inventory_df.append(add_item(item=item[0],category=category),ignore_index=True)
-
-        inventory_df.loc[inventory_df['Item'] == item[0], 'Qty'] += int(item[1])
-
-
-    loaded(path, 'Restocks')
-    inventory_df.to_csv('inventory.csv', index=False)
-    unfulfilled_orders.to_csv('unfulfilled.csv', index=False)
-
-    return inventory_df
-
-def stock_status_mapper(qty):
-    if qty>5:
-        return 'In Stock'
-    if qty<=5 and qty>0:
-        return 'Low Stock'
-    if qty==0:
-        return 'Out of Stock. Check "unfulfilled.csv". '
-
-
-def update_inventory():
-    if not os.path.exists('order.csv') and not os.path.exists('restock.csv'):
-        print('No new orders or restock found. Printing Inventory...')
-
-    if os.path.exists('order.csv'):
-        read_order()
-    if os.path.exists('restock.csv'):
-        read_restock()
-    inventory=pd.read_csv('inventory.csv')
-    inventory['Notes']=inventory['Qty'].map(stock_status_mapper)
-    inventory= inventory.sort_values(by='Notes', ascending=False)
-    inventory.to_csv('inventory.csv', index=False)
-    return inventory
-
-def test_function():
-    gen.generate_test()
-    print(update_inventory())
-
-test_function()
-
+inv = Inventory()
+order = reader.order_from_csv("examples/restock.csv")
+inv.fulfill_order(order)
